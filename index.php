155b6c267ce972b798fd7ba881fc8d072f231449
ValentinaNovarino <valenovarino@gmail.com>
2020-12-14 16:19:08 +0100

    aggiunto lettura lunghezza e tipo con var_dump

--- index.php
+++ index.php
@@ -6,17 +6,18 @@
     </head>
     <body>
         <p>
-            PARAGRAFO ORIGINALE: 
+            PARAGRAFO ORIGINALE:
             <?php
 
             $paragrafo = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
 
-            echo $paragrafo;
+            var_dump($paragrafo);
+            // echo $paragrafo;
 
             ?>
         </p>
         <p>
-            Badword:
+            BADWORD:
             <?php
 
             $badword = $_GET['badword'];
@@ -26,7 +27,7 @@
             ?>
         </p>
         <p>
-            Paragrafo censurato:
+            PARAGRAFO CENSURATO:
             <?php
 
             $paragrafoCensurato = str_replace($badword, '***', $paragrafo);
@@ -36,7 +37,7 @@
             ?>
         </p>
         <p>
-            Il paragrafo ha:
+            Il paragrafo ha
             <?php
 
             $lunghezzaParagrafo = strlen($paragrafo);
