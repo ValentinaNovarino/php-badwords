0815d5977e986dfc8a8b16c3bac5c3c917fa28ae
ValentinaNovarino <valenovarino@gmail.com>
2020-12-14 15:47:21 +0100

    aggiunta badword

--- index.php
+++ index.php
@@ -5,13 +5,25 @@
         <title></title>
     </head>
     <body>
+        <h1>PARAGRAFO ORIGINALE</h1>
         <p>
             <?php
 
             $paragrafo = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
 
             echo $paragrafo;
-            
+
+            ?>
+        </p>
+        <h2>
+            BADWORD
+        </h2>
+        <p>
+            <?php
+
+            $badword = $_GET['badword'];
+
+            echo $badword;
             ?>
         </p>
     </body>
