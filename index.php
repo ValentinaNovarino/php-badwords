e549dd29b909952b92ba4197312e980ffe60b1f4
ValentinaNovarino <valenovarino@gmail.com>
2020-12-14 16:10:11 +0100

    stampato lunghezza paragrafo

--- index.php
+++ index.php
@@ -5,8 +5,8 @@
         <title></title>
     </head>
     <body>
-        <h1>PARAGRAFO ORIGINALE</h1>
         <p>
+            PARAGRAFO ORIGINALE: 
             <?php
 
             $paragrafo = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
@@ -28,10 +28,23 @@
         <p>
             Paragrafo censurato:
             <?php
+
             $paragrafoCensurato = str_replace($badword, '***', $paragrafo);
 
             echo $paragrafoCensurato;
+
             ?>
+        </p>
+        <p>
+            Il paragrafo ha:
+            <?php
+
+            $lunghezzaParagrafo = strlen($paragrafo);
+
+            echo $lunghezzaParagrafo;
+
+            ?>
+            caratteri
         </p>
     </body>
 </html>
